790a9623da3a39556ad8d41897acde4091b80d6b
naolumer <naolumer3@gmail.com>
2024-12-24 23:09:51 +0300

    move-zeroes

---
+++ Leetcode Easy/move-zeroes.py
@@ -0,0 +1,8 @@
+def moveZeroes(nums):
+    
+    pointer = 0
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            nums[pointer],nums[i] = nums[i],nums[pointer]
+            pointer+=1
+    return nums
