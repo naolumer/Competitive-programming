860efbff3fed8ed92d07e850a99607859954f2e0
naolumer <naolumer3@gmail.com>
2024-12-27 21:55:56 +0300

    pow(x,n) iterative solution

--- Leetcode Medium/pow(x,n).py
+++ Leetcode Medium/pow(x,n).py
@@ -1,11 +1,35 @@
 class Solution:
     def myPow(self,x,n):
-        if not n:
-            return 1
-        if n<0:
-            return 1/self.myPow(x,-n)
+
+        #   Recursive solution
+
+        # if not n:
+        #     return 1
+        # if n<0:
+        #     return 1/self.myPow(x,-n)
         
-        if n%2:     #checking if the exponent is odd
-            return x *self.myPow(x,n-1)
-        return self.myPow(x*x,n/2)
+        # if n%2:     #checking if the exponent is odd
+        #     return x *self.myPow(x,n-1)
+        # return self.myPow(x*x,n/2)
+    
+    
+        # Iterative Solution
+
+        
+        if n < 0:
+            n = -n
+            x= 1/x
+        
+        poww = 1
+
+        while n:
+            if n%2:
+                poww*=x
+            x*=x
+            n//=2
+        return poww
+res = Solution()
+print(res.myPow(4,3))
+                
+        
 
