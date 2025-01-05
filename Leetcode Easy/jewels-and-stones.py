8a9b8ae05f2fc636cf9560e0d6d66a92ab189c6b
naolumer <naolumer3@gmail.com>
2025-01-05 19:44:58 +0300

    jewels-and-stones

---
+++ Leetcode Easy/jewels-and-stones.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count_s = Counter(stones)
+        ans = 0
+        for char,count in count_s.items():
+            if char in jewels:
+                ans+=count
+        return ans
