beebbacaff6b74fc3c47437cd9dbe9ace1a4bb07
naolumer <naolumer3@gmail.com>
2025-01-12 22:27:16 +0300

    Leetcode[2116]

---
+++ Leetcode Medium/check-if-a-parenthesis-string-can-be-valid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+
+        open_ = []
+        lock = []
+        for i in range(len(s)):
+            if locked[i] == '0':
+                open_.append(i)
+            elif s[i] == '(':
+                lock.append(i)
+            else:
+                if lock:
+                    lock.pop()
+                elif open_:
+                    open_.pop()
+                else:
+                    return False
+        
+        while open_ and lock and lock[-1] < open_[-1]:
+            open_.pop()
+            lock.pop()
+        
+        if lock or len(open_) % 2 == 1:
+            return False
+
+        return True
