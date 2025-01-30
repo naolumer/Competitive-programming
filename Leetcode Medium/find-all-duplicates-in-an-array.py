39245db705246af0677931fcaa8addfc92efdbda
naolumer <naolumer3@gmail.com>
2025-01-30 22:21:49 +0300

    Leetcode-442

---
+++ Leetcode Medium/find-all-duplicates-in-an-array.py
@@ -0,0 +1,10 @@
+from collections import Counter
+from types import List
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        arr= []
+        for num,frq in count.items():
+            if frq==2:
+                arr.append(num)
+        return arr
