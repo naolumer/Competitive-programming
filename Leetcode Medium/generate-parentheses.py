42f118505cbe58fbe066e514f4a8843f683bc8de
naolumer <naolumer3@gmail.com>
2025-02-09 19:29:05 +0300

    Leetcode-22

---
+++ Leetcode Medium/generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        stack,result = [],[]
+
+        def backtrack(o,c):
+
+            if len(stack)==n*2:
+                result.append("".join(stack))
+                return
+            
+            if o < n:
+                stack.append("(")
+                backtrack(o+1,c)
+                stack.pop()
+            
+            if c < o:
+                stack.append(")")
+                backtrack(o,c+1)
+                stack.pop()
+        backtrack(0,0)
+        return result
