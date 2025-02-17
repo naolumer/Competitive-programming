71ddb7185c90a022cecb048a9f54a0046a34cb81
naolumer <naolumer3@gmail.com>
2025-02-17 23:07:11 +0300

    Leetcode-2723

---
+++ 30 days javascript/2723. Add Two Promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+const addTwoPromises = async (promise1, promise2)=> {
+    let [prom1,prom2] = await Promise.all([promise1,promise2])
+    return prom1+prom2
+    
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
