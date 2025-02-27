477be29d11a8a8c04e5b82c3ed7b2a238e182a41
naolumer <naolumer3@gmail.com>
2025-02-27 23:39:24 +0300

    imported defaultdict and fixed indentation error

--- Leetcode Medium/path-sum-III.py
+++ Leetcode Medium/path-sum-III.py
@@ -1,18 +1,19 @@
+from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
 	
-	    sums = defaultdict(int)
-	    sums[0] = 1
+        sums = defaultdict(int)
+        sums[0] = 1
 
-	    def dfs(root, total):
-             count = 0
-             if root:
+        def dfs(root, total):
+            count = 0
+            if root:
                 total += root.val
                 count = sums[total-targetSum]
                 sums[total] += 1
                 count += dfs(root.left, total) + dfs(root.right, total)
                 sums[total] -= 1
 
-             return count
+            return count
 
-	    return dfs(root, 0) 
+        return dfs(root, 0) 
