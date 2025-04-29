5327dd2c85844e3284de9225b095d68d9ee25583
naolumer <naolumer3@gmail.com>
2025-04-29 21:59:53 +0300

    updated

--- Leetcode Medium/group-anagrams.py
+++ Leetcode Medium/group-anagrams.py
@@ -1,10 +1,7 @@
 from collections import defaultdict
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+     
         hashmap = defaultdict(list)
 
         for s in strs:
