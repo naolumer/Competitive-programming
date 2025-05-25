d1efa4dc632ddb056c025eea18b327641039947b
naolumer <naolumer3@gmail.com>
2025-05-25 22:00:21 +0300

    rotate image

--- Leetcode Medium/rotate-image.py
+++ Leetcode Medium/rotate-image.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def rotate(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
         n = len(matrix)
 
         for i in range(n):
