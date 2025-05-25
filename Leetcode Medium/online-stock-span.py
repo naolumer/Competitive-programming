723f540b58680225adcf6df4fdb93addf48f294d
naolumer <naolumer3@gmail.com>
2025-05-25 22:00:53 +0300

    stockspanner

--- Leetcode Medium/online-stock-span.py
+++ Leetcode Medium/online-stock-span.py
@@ -3,7 +3,6 @@ class StockSpanner:
     def __init__(self):
         self.prices = []
         
-
     def next(self, price: int) -> int:
         span = 1
         while self.prices and self.prices[-1][0] <= price:
