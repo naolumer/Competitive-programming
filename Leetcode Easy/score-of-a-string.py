5131d2a23588ed2c3cceb0c968a43358dbcb8f26
naolumer <naolumer3@gmail.com>
2025-06-13 17:40:24 +0300

    updated

--- Leetcode Easy/score-of-a-string.py
+++ Leetcode Easy/score-of-a-string.py
@@ -1,9 +1,7 @@
 class Solution(object):
+    
     def scoreOfString(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        
         res = 0
         for i in range(1,len(s)):
             res+= abs(ord(s[i-1])-ord(s[i]))
