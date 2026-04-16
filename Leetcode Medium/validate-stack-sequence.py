c5eccf50278f363f7bdd3cfbeb324e51d204772c
naolumer <naolumer3@gmail.com>
2026-04-16 21:51:19 +0300

    validate-stack-sequence

---
+++ Leetcode Medium/validate-stack-sequence.py
@@ -0,0 +1,10 @@
+def validateStackSequences(pushed, popped):
+    i=0
+    stack = []
+
+    for n in pushed:
+        stack.append(n)
+        while i<len(popped) and stack and popped[i] == stack[-1]:
+            stack.pop()
+            i+=1
+    return not stack
